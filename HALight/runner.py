d6b41f27ee0d3c72a4598ad823e8418c1c7bc27b
Chenkai Hu <70884894+Huchenkaii@users.noreply.github.com>
2026-06-02 21:31:03 +0800

    runner.py

--- HALight/runner.py
+++ HALight/runner.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 from utils.get_neighbour import build_neighbors_from_file
 import random
 import numpy as np
